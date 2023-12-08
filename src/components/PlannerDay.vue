97e07272b815a8f8697b5e0a762a13d9e315f662
HippiHo <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>
2023-12-08 12:14:02 +0100

    feat (day): adds visibility toggle

--- src/components/PlannerDay.vue
+++ src/components/PlannerDay.vue
@@ -1,43 +1,60 @@
 <script setup lang="ts">
-import type { Day } from '../types/calendar.types'
+import { ref } from "vue";
+import type { Day } from "../types/calendar.types";
 defineProps<{
-    day: Day
-}>()
+  day: Day;
+}>();
+
+const isVisible = ref<boolean>(true);
+const toggleDayOpacity = () => {
+  isVisible.value = !isVisible.value;
+};
 </script>
 
 <template>
-    <div class="day" :class="{
-        weekend: day.isWeekend,
-        workDay: !day.isWeekend
-    }">
-        <span class="weekDay">{{ day.weekDay }}</span>
-        <span class="number">{{ day.number }}</span>
-    </div>
+  <div
+    class="day"
+    :class="{
+      weekend: day.isWeekend,
+      workDay: !day.isWeekend,
+      hidden: !isVisible,
+    }"
+    @click="toggleDayOpacity"
+  >
+    <span class="weekDay">{{ day.weekDay }}</span>
+    <span class="number">{{ day.number }}</span>
+  </div>
 </template>
 
 <style lang="scss" scoped>
 .day {
-    border: 1px solid black;
-    height: 100px;
-    padding: 8px 32px 8px 8px;
+  border: 1px solid black;
+  height: 100px;
+  opacity: 1;
+  padding: 8px 32px 8px 8px;
+  transition: opacity linear 0.2s;
 
-    .weekDay {
-        display: block;
-    }
+  .weekDay {
+    display: block;
+  }
 
-    .number {
-        font-weight: 700;
-        display: block;
-        text-align: right;
-        font-size: 26px;
-    }
+  .number {
+    font-weight: 700;
+    display: block;
+    text-align: right;
+    font-size: 26px;
+  }
 }
 
 .workDay {
-    background-color: lightsalmon;
+  background-color: lightsalmon;
 }
 
 .weekend {
-    background-color: lightgray;
+  background-color: lightgray;
+}
+
+.hidden {
+  opacity: 0.2;
 }
 </style>
